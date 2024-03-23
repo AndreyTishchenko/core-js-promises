92c33cacefebc711ed0de01578064519b21091f5
AndreyTishchenko <Tishchenko.Andrew@yandex.ru>
2024-03-23 21:23:58 +0400

    New commit

--- src/core-js-promises-tasks.js
+++ src/core-js-promises-tasks.js
@@ -18,7 +18,7 @@
  * 1    => promise that will be fulfilled
  */
 function getPromise(number) {
-  const promice = new Promise(function (resolve, reject) {
+  const promice = new Promise(function gjvhj(resolve, reject) {
     if (number >= 0) {
       resolve(number);
     } else {
